a441bb0b99bb62d6ecd17eb1e7e356c7f4fd4650
biophys <blaukarat@web.de>
2019-06-28 13:44:15 +0200

    update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -28,7 +28,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
 
-MOCK_MODULES = ['impro']
+MOCK_MODULES = ['pycuda',]
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- General configuration ------------------------------------------------
